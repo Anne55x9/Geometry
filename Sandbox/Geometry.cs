6dc34a423141562377dfdca0f04f19eb20b57b02
anne55x9 <anne55x9@edu.easj.dk>
2016-10-26 12:03:42 +0200

    Exercise 12 done

--- Sandbox/Geometry.cs
+++ Sandbox/Geometry.cs
@@ -7,5 +7,65 @@ namespace Sandbox
 {
     public class Geometry
     {
+        public double length { get; set; }
+        public double width { get; set; }
+        public double perimeter { get; set; }
+        public double area { get; set; }
+        public double pi { get; private set; }
+        public double radius { get; set; }
+        public double circleArea { get; set; }
+        public Boolean polygon;
+        public double angle { get; set; }
+        
+
+        public Geometry(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public double GetPerimeter()
+        {
+            perimeter = 2*(length + width);
+            return perimeter; 
+        }
+
+        public double GetArea()
+        {
+            area = length * width;
+            return area;
+        }
+
+        public Geometry(double radius)
+        {
+            this.pi = 3.14;
+            this.radius = radius;
+        }
+
+
+        public double GetCircleArea()
+        {
+            circleArea = pi * (radius * 2);
+            return circleArea;
+        }
+
+        public Geometry()
+        {
+           
+        }
+
+        public Boolean GetPolygon(double angle)
+        {
+
+            Boolean result = true;
+            if (angle != 90)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+
     }
 }
--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -8,6 +8,19 @@ namespace Sandbox
         {
             // The FIRST line of code should be BELOW this line
 
+            Geometry rectangle = new Geometry(3.5,6.4);
+
+            Console.WriteLine($"The perimeter of the rectangle is: {rectangle.GetPerimeter()}");
+
+            Console.WriteLine($"The area of the rectangle is: {rectangle.GetArea()}");
+
+            Geometry circle = new Geometry(5.8);
+
+            Console.WriteLine($"The area of the circle is: {circle.GetCircleArea()}");
+
+            Geometry polygonR = new Geometry();
+
+            Console.WriteLine($"The polygon is a rectagle: {polygonR.GetPolygon(60)}");
 
             // The LAST line of code should be ABOVE this line
         }
